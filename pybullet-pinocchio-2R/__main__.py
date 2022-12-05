2570a95b65a303f79fec5b374aae548758fe94ce
FrancescoD98 <francy.dorazio98@gmail.com>
2022-12-05 17:06:23 +0100

    ee position control added

--- pybullet-pinocchio-2R/__main__.py
+++ pybullet-pinocchio-2R/__main__.py
@@ -8,6 +8,7 @@ import controller
 
 simDT = 0.005 # simulation timestep
 simTime = 10 # total simulation time in seconds
+q0 = np.array([0.5, 0.5]) # initial configuration
 
 robotID, robotModel = sim_utils.simulationSetup(simDT)
 
@@ -16,6 +17,8 @@ nDof = 2
 # we are going to consider both revolute joints, so we fill the whole joint indices list
 jointIndices = range(nDof)
 
+for i in jointIndices:
+    pb.resetJointState(robotID, i, q0[i])
 
 q, qdot = sim_utils.getState(robotID, jointIndices) 
 # in general we need to call this to compute all the kinematic and dynamic quantities (see pinocchio docs)
@@ -23,7 +26,7 @@ pin.computeAllTerms(robotModel.model, robotModel.data, q, qdot)
 
 # set a desired joint configuration
 qdes = np.array([np.pi/2,-np.pi/2])
-
+ee_des = np.array([0, 0.3])
 
 input("press ENTER to START the simulation:")
 
@@ -31,12 +34,15 @@ for i in range(int(simTime/simDT)):
 
     # read the current joint state from the simulator
     q, qdot = sim_utils.getState(robotID, jointIndices)    
-
-    print("error = ", q-qdes)
-    print(controller.computeEEpose(robotModel, q))
+    
+    # ee_pos = controller.computeEEpose(robotModel, q)
+    # print(ee_pos.translation)
+    # print("error = ", q-qdes)
+    # print(controller.computeEEpose(robotModel, q))
 
     # compute the feedback torque command
-    tau = controller.JointPositionControl(robotModel, q, qdot, qdes)
+    # tau = controller.JointPositionControl(robotModel, q, qdot, qdes)
+    tau = controller.EePositionController(robotModel, q, qdot, ee_des)
 
     # send the torque command to the simulator
     pb.setJointMotorControlArray(robotID, jointIndices, controlMode = pb.TORQUE_CONTROL, forces = tau)
--- pybullet-pinocchio-2R/controller.py
+++ pybullet-pinocchio-2R/controller.py
@@ -2,18 +2,49 @@ import numpy as np
 
 import pinocchio as pin
 
-KP = 50
-KD = 5
-
 def JointPositionControl(robotModel, q, qdot, qdes):
     """
     Joint position controller implemented via inverse dynamics + PD
     """
 
+    KP = 50
+    KD = 5
+    
     qddot_des = KP*(qdes-q) + KD*(-qdot)    
     torque = pin.rnea(robotModel.model, robotModel.data, q, qdot, qddot_des)
 
     return torque
+
+def EePositionController(robotModel, q, qdot, ee_des):
+
+    KP = np.array([[50, 0],[0, 50]])
+    KD = np.array([[5, 0],[0, 5]])
+
+    # compute end effector position
+    ee_homo = computeEEpose(robotModel, q)
+    ee_pos = ee_homo.translation[1:3]
+
+    ### compute the geometric Jacobian J and dJ/dt
+    # update the joint placements according to the current joint configuration
+    pin.forwardKinematics(robotModel.model, robotModel.data, q)
+    # Computes the full model Jacobian
+    pin.computeJointJacobians(robotModel.model, robotModel.data, q)
+    # updates the position of each frame contained in the mode
+    pin.updateFramePlacements(robotModel.model, robotModel.data)
+    # get ID of the end effector
+    EEid = robotModel.model.getFrameId("end_effector")
+    # Jacobian of the end effector and its derivative
+    Jee = pin.getFrameJacobian(robotModel.model, robotModel.data, EEid, pin.ReferenceFrame.LOCAL)[1:3]
+    JeeDot = pin.computeJointJacobiansTimeVariation(robotModel.model, robotModel.data, q, qdot)[1:3]
+    
+
+    # compute the desired accelleration of the end effector
+    ee_acc = KP@(ee_des - ee_pos) + KD@(-np.dot(Jee, qdot))
+
+    # desired joint acceleration
+    qddot_des = np.linalg.pinv(Jee)@(ee_acc - JeeDot@qdot)
+
+    return pin.rnea(robotModel.model, robotModel.data, q, qdot, qddot_des)
 
 
 def computeEEpose(robotModel, q):
