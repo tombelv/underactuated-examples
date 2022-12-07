62b667a075bd3302ea8f0351694b95b024914ed0
tombelv <tommaso.belv@gmail.com>
2022-12-07 11:29:18 +0100

    changed main code structure

---
+++ pybullet-pinocchio-2R/simulation.py
@@ -0,0 +1,64 @@
+import pybullet as pb
+import time
+import numpy as np
+import pinocchio as pin
+
+import sim_utils
+import controller
+
+def simulate():
+
+    simDT = 0.005 # simulation timestep
+    simTime = 100 # total simulation time in seconds
+    q0 = np.array([0.5, 0.5]) # initial configuration
+
+    robotID, robotModel = sim_utils.simulationSetup(simDT)
+
+    nDof = 2
+
+    # we are going to consider both revolute joints, so we fill the whole joint indices list
+    jointIndices = range(nDof)
+
+    for i in jointIndices:
+        pb.resetJointState(robotID, i, q0[i])
+
+    q, qdot = sim_utils.getState(robotID, jointIndices) 
+    # in general we need to call this to compute all the kinematic and dynamic quantities (see pinocchio docs)
+    pin.computeAllTerms(robotModel.model, robotModel.data, q, qdot)
+
+    # set a desired joint configuration
+    qdes = np.array([np.pi/2,-np.pi/2])
+    ee_des = np.array([0, 0.3])
+
+    input("press ENTER to START the simulation:")
+
+    for i in range(int(simTime/simDT)):
+
+        # read the current joint state from the simulator
+        q, qdot = sim_utils.getState(robotID, jointIndices)    
+
+        # ee_pos = controller.computeEEpose(robotModel, q)
+        # print(ee_pos.translation)
+        # print("error = ", q-qdes)
+        # print(controller.computeEEpose(robotModel, q))
+
+        # compute the feedback torque command
+        #tau = controller.JointPositionControl(robotModel, q, qdot, qdes)
+        tau = controller.EePositionController(robotModel, q, qdot, ee_des)
+
+        # send the torque command to the simulator
+        pb.setJointMotorControlArray(robotID, jointIndices, controlMode = pb.TORQUE_CONTROL, forces = tau)
+
+        # advance the simulation one step
+        pb.stepSimulation()
+        time.sleep(simDT)
+
+
+    input("press ENTER to END the simulation:")
+
+    pb.disconnect()
+
+
+
+
+
